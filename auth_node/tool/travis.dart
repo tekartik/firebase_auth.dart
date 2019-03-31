56b45dd20d49a7665a251d60a55875f48661fba2
alex <alex@tekartik.com>
2019-03-31 10:14:31 +0200

    build: version

--- auth_node/tool/travis.dart
+++ auth_node/tool/travis.dart
@@ -1,6 +1,6 @@
 import 'package:process_run/shell.dart';
 import 'package:process_run/which.dart';
-import 'run_node_test.dart' as run_node_test;
+import 'build_node_test.dart' as build_node_test;
 import 'setup_node.dart' as setup_node;
 
 Future main() async {
@@ -9,12 +9,10 @@ Future main() async {
   await shell.run('''
 dartanalyzer --fatal-warnings --fatal-infos lib test
 
-# pub run build_runner test -- -p vm,chrome
-# pub run test -p vm,chrome
 ''');
 
   if (whichSync('npm') != null) {
     await setup_node.main();
-    await run_node_test.main();
+    await build_node_test.main();
   }
 }
