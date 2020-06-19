73435e33e26b382ef9aca50a1471017509dbad50
alex <alex@tekartik.com>
2020-06-19 18:05:50 +0200

    fix: lint

--- auth_jwt/test/auth_jwt_impl_test.dart
+++ auth_jwt/test/auth_jwt_impl_test.dart
@@ -1,4 +1,3 @@
-import 'package:corsac_jwt/corsac_jwt.dart';
 import 'package:tekartik_firebase_auth_jwt/auth_jwt.dart';
 import 'package:tekartik_firebase_auth_jwt/src/auth_info.dart';
 import 'package:tekartik_firebase_auth_jwt/src/database.dart';
