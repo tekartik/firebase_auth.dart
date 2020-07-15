c8691fe24ee810d3174191be0539064782102065
alex <alex@tekartik.com>
2020-07-15 17:04:16 +0200

    feat: browser facebook login

--- auth_browser/lib/auth_browser.dart
+++ auth_browser/lib/auth_browser.dart
@@ -3,6 +3,9 @@ import 'package:tekartik_firebase_auth_browser/src/auth_browser.dart'
     as auth_browser;
 export 'package:tekartik_firebase_auth_browser/src/auth_browser.dart'
     show GoogleAuthProvider;
+export 'package:tekartik_firebase_auth_browser/src/auth_browser_facebook.dart'
+    show FacebookAuthProvider, FacebookAuthCustomParameters;
+
 export 'auth_browser_api.dart';
 
 AuthService get authServiceBrowser => auth_browser.authService;
