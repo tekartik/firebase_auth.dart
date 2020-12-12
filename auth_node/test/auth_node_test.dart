502367a5697e5fe8a675b6e01b7efdac66d212fd
alex <alex@tekartik.com>
2020-12-12 12:30:16 +0100

    fix: skip node test if no config

--- auth_node/test/auth_node_test.dart
+++ auth_node/test/auth_node_test.dart
@@ -1,13 +1,26 @@
 @TestOn('node')
 library tekartik_firebase_auth_node.test.auth_node_test;
 
+import 'package:tekartik_app_node_utils/node_utils.dart';
 import 'package:tekartik_firebase_auth_node/auth_node.dart';
 import 'package:tekartik_firebase_auth_test/auth_test.dart';
 import 'package:tekartik_firebase_node/firebase_node.dart';
 import 'package:test/test.dart';
 
+var _env = platform.environment;
+
 void main() {
   var firebase = firebaseNode;
   var authService = authServiceNode;
-  run(firebase: firebase, authService: authService);
+
+  test('app', () {
+    print('FIREBASE_CONFIG: ${_env['FIREBASE_CONFIG']}');
+    print(
+        'GOOGLE_APPLICATION_CREDENTIALS: ${_env['GOOGLE_APPLICATION_CREDENTIALS']}');
+  });
+  if (_env['FIREBASE_CONFIG'] != null) {
+    group('auth_node', () {
+      run(firebase: firebase, authService: authService);
+    });
+  }
 }
