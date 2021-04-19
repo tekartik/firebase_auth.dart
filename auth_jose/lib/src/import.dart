59f12575dbe0dc874e170ad49f7a098a3fc4e013
alex <alex@tekartik.com>
2021-04-19 08:52:18 +0200

    delete auth_jose

--- auth_jose/lib/src/import.dart
+++
@@ -1,1 +0,0 @@
-export 'package:tekartik_common_utils/common_utils_import.dart';
--- auth_jose/lib/src/scopes.dart
+++
@@ -1,4 +0,0 @@
-const firebaseGoogleApisUserEmailScope =
-    'https://www.googleapis.com/auth/userinfo.email';
-const firebaseGoogleApisFirebaseDatabaseScope =
-    'https://www.googleapis.com/auth/firebase.database';
